defc40f41c00b9b36b480deaa22087930927baf5
Tien <ankyney@gmail.com>
2019-10-30 14:46:56 +0700

    update

--- Shop/src/main/java/com/tthshop/repository/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tthshop.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.tthshop.entities.Order;
-
-public interface OrderRepo extends CrudRepository<Order, Integer>  {
-
-}
